8401075cfae10d93ce2d7463f9bc0983cd9d6a85
Cotezzo <46570831+Cotezzo@users.noreply.github.com>
2019-01-14 21:33:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,13 +15,10 @@ $url = "https://api.telegram.org/$api/sendMessage?chat_id=".$chatid."&text=".url
 $get = file_get_contents($url);
 }
 
-$x = null;
-
 switch($message)
 {
   case "/start":
-    #ob_flush();
-     #flush();
+    static $x = null
     sendMessage ($chatid, "Tra 10 minuti cominciano i giochi");
     sleep(10);
     sendMessage ($chatid, "Cominciano i giochi!");
@@ -29,13 +26,13 @@ switch($message)
     sendMessage ($chatid, "Parola: C_ _ _ _ _ O"); 
     break;    
   case "Cavallo":
-    if (!isset($x)) {
+    if (empty($x)) {
     sendMessage ($chatid, "Hai indovinato!");
-    $GLOBALS['x'] = 3;
+    static $x = 3;
     }
     break;   
   default:
-    if (!isset($x)) {
+    if (empty($x)) {
     sendMessage ($chatid, "Hai sbagliato!");
     } else {
     sendMessage ($chatid, "Basta inviare messaggi!");
@@ -44,11 +41,8 @@ switch($message)
 
 sendMessage ($chatid, $x);
 
-if ($x == 3) {
-  static $x = 3;
-}
 
-if (!isset($x)) {
+if (empty($x)) {
 goto indizio1;
 }
 
